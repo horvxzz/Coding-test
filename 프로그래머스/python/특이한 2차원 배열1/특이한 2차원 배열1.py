56ec0d3997b83878e7dee6302ebe4cb0b07e053f
전혜린 <s25068@gsm.hs.kr>
2026-04-06 08:33:11 +0900

    2026.04.06

---
+++ 프로그래머스/python/특이한 2차원 배열1/특이한 2차원 배열1.py
@@ -0,0 +1,11 @@
+def solution(n):
+    str1 = []
+    for i in range(n):
+        row = []
+        for j in range(n):
+            if i == j:
+                row.append(1)
+            else:
+                row.append(0)
+        str1.append(row)
+    return str1
